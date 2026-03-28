152e6e704132aa5ad36abe1d5d609ca192da5680
Pooja-47 <poojakanwar5117@gmail.com>
2026-03-28 17:29:43 +0530

    Day4: Operators and simple calculator.

---
+++ day4_operators/Day4.py
@@ -0,0 +1,24 @@
+# Operators:- 
+print() 
+a=5
+b=3
+print("Addition:",a+b)    # Add +
+print("Subtraction:",a-b) # Subtract -
+print("Multiplication:",a*b) # Product *
+print("Division:",a/b) # Quotient /
+print("Modulous:",a%b) # Remainder %
+print("Floor division:",a//b) # Quotient (value before point) //
+print("Exponential:",a**b) # To the Power ** (a*a*a)
+
+#These are various operators used in python to perform arithmetic operations.
+print()
+# CREATE A CALCULATOR:-
+
+a=2
+b=3
+print("The value of",a,"+",b,"is=" ,a+b)
+print("The value of",a,"-",b,"is=" ,a-b)
+print("The value of",a,"*",b,"is=" ,a*b)
+print("The value of",a,"/",b,"is=" ,a/b)
+
+
